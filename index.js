12f6751c3f5380418253fdbddec1384113d49b85
hrefdev <href.dev@gmail.com>
2021-11-29 22:56:38 +0700

    add index for qr code

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-import express from "express";
+import { createRequire } from "module"; // agara tetap bisa memakai require()
+import express, { response } from "express";
 import db from "./config/Database.js";
 import cookieParser from "cookie-parser";
 import router from "./routes/index.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http"
+import qrcode from "qrcode"
+import { Client } from "whatsapp-web.js"
+import fs from "fs"
+
+const require = createRequire(import.meta.url);
+const socketIO = require('socket.io')
 dotenv.config()
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+
+/* ---------Handel API Whatsapp--------- */
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require('./whatsapp-session.json');
+}
+
+const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    sessionCfg=session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.initialize();
+/* ------End Handle API Whatsapp------ */
 
 // memastikan koneksi database berjalan dnegan baik
 try {
@@ -15,9 +48,37 @@ try {
     console.error(error)
 }
 
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({
+    credentials:true,
+    origin:[
+        'http://localhost:3000',
+        'http://192.168.100.19:5000/users'
+    ]
+}));
 app.use(cookieParser());
 app.use(express.json())
 app.use(router)
+app.use(express.urlencoded({ extended:true }))
+// socket io
+io.on('connection', function(socket){
+    socket.emit('message', 'Connecting...')
+    client.on('qr', (qr) => {
+        // Generate and scan this code with your phone
+        console.log('QR RECEIVED', qr);
+        qrcode.toDataURL(qr,(err, url) => {
+            socket.emit('qr', url)
+            socket.emit('message', 'QR Code Ready')
+        })
+        client.on('ready', () => {
+            socket.emit('message', 'Whatsapp ready to use')
+        });
+    });
+})
 
-app.listen(5000, () => console.log('server running at port 5000'))
+// send OTP
+export const sendOTP = (nohp, otp) => {  
+    const msg = `KARKOON - Kode OTP anda : ${otp}. Kode ini hanya berlaku selama 2 menit. Terima kasih`
+        client.sendMessage(nohp, msg)
+}
+
+server.listen(5000, () => console.log('server running at port 5000'))
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ import { getOtp, verifyOtp } from "../controllers/Otp.js";
 import { Token, refreshToken } from "../controllers/Token.js";
 import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
-const router = express.Router()
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const router = express.Router()
 router.get('/users', verifyToken,  getUsers)
 router.post('/register', Register)
 router.post('/login', Login)
@@ -13,6 +17,10 @@ router.post('/verifyotp', verifyOtp)
 router.get('/token', Token)
 router.get('/refreshToken', refreshToken)
 router.delete('/logout', Logout)
+router.get('/', (req, res) =>{
+    // res.sendFile(new URL('../views/index.html', {root:__dirname}));
+    res.sendFile('./index.html', {root:__dirname})
+})
 
 
 export default router;
