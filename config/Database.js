7758451bc5bd63c28f56878b2a99e1a19833b724
hrefdev <href.dev@gmail.com>
2021-11-30 15:21:24 +0700

    setting db heroku

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth-waapi', 'root', '', {
-    host:'localhost',
+const db = new Sequelize('heroku', 'b73eb35faec2aa', '1da236ba', {
+    host:'us-cdbr-east-04.cleardb.com',
     dialect:'mysql',
-    dialectOptions:{
-        useUTC:false
-    },
     timezone: '+07:00'
 });
+// const db = new Sequelize('auth-waapi', 'root', '', {
+//     host:'localhost',
+//     dialect:'mysql',
+//     timezone: '+07:00'
+// });
 
 export default db;
